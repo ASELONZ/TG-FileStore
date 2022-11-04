42e23c67b68bb0a5426de8c30b6a4318e10c6856
ASELONZ <93417104+ASELONZ@users.noreply.github.com>
2022-11-04 13:00:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -385,7 +385,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 user = await bot.get_chat_member(channel_chat_id, cmd.message.chat.id)
                 if user.status == "kicked":
                     await cmd.message.edit(
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/greymatters_bots_discussion).",
+                        text="Sorry Sir, You are Banned to use me. Contact my [Owner](https://t.me/defenderofthemultiverse_bot).",
                         disable_web_page_preview=True
                     )
                     return
@@ -407,7 +407,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 return
             except Exception:
                 await cmd.message.edit(
-                    text="Something went Wrong. Contact my [Support Group](https://t.me/greymatters_bots_discussion).",
+                    text="Something went Wrong. Contact my [Owner](https://t.me/ANKIT 3690) [Owner](https://t.me/Saurav3BV6SA9LLElon7Musk.",
                     disable_web_page_preview=True
                 )
                 return
